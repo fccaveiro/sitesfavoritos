abba04b6241d2c689edd8fc590fabc5976227506
Alexandre Nunes <alexandre.nunes@flipkick.cloud>
2018-05-19 18:04:31 +0100

    Add Icon

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
-function add(){
-    localStorage.setItem("myList",localStorage.getItem("myList")+"|"+Math.floor(Math.random() * 9999999)+";"+document.getElementById("name").value+";http://"+document.getElementById("url").value);
-    document.getElementById("name").value="";
-    document.getElementById("url").value="";
+function add() {
+    localStorage.setItem("myList", localStorage.getItem("myList") + "|" + Math.floor(Math.random() * 9999999) + ";" + document.getElementById("name").value + ";http://" + document.getElementById("url").value);
+    document.getElementById("name").value = "";
+    document.getElementById("url").value = "";
 }
-setInterval(function(){
-    var lis=document.getElementById("list");
-    if(localStorage.getItem("myList")){
-        lis.innerHTML="";
-        var myList=localStorage.getItem("myList");
-        myList.split('|').forEach(function(tmp) {
-            if(tmp!="null"){
-                var tmp1=tmp.split(';');
-                lis.innerHTML+="<div id='"+tmp1[0]+"'>"+tmp1[1]+"<a style='position:absolute;right:60px;' target='_blank' href='"+tmp1[2]+"'>Abrir</a></div>";
+setInterval(function () {
+    var lis = document.getElementById("list");
+    if (localStorage.getItem("myList")) {
+        lis.innerHTML = "";
+        var myList = localStorage.getItem("myList");
+        myList.split('|').forEach(function (tmp) {
+            if (tmp != "null") {
+                var tmp1 = tmp.split(';');
+                lis.innerHTML += "<div id='" + tmp1[0] + "'>" + tmp1[1] + "<a style='position:absolute;right:60px;' target='_blank' href='" + tmp1[2] + "'>Abrir</a></div>";
             }
-        });     
+        });
     }
-},300);
+}, 300);
